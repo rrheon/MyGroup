b3f439d15f21d0034b6303268a0ea032c6424aca
rrheon <yongheon0806@gmail.com>
2025-06-23 14:30:43 +0900

    Initial Commit

---
+++ MyCalc/MyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalcApp.swift
+//  MyCalc
+//
+//  Created by 최용헌 on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
